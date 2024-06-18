5feb1a139ecc0af60cc717cd9e73941efb6d4e29
Mostafa Elsayed <mostafa.elsayed.2002@gmail.com>
2024-06-18 19:42:02 +0300

    Add: Post page

---
+++ app/Post/[id]/page.tsx
@@ -0,0 +1,52 @@
+// pages/[id].js
+"use client";
+import { useEffect, useState } from "react";
+
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+export default function Page({ params }: { params: { id: string } }) {
+  const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(
+          ` https://jsonplaceholder.typicode.com/posts/${params.id}`
+        );
+        if (res.status === 404) return;
+
+        const data = await res.json();
+
+        setPost(data);
+        setLoading(false);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <main>
+      <div className="flex flex-col items-center gap-5 p-5">
+        {loading ? (
+          <Skeleton />
+        ) : (
+          <div
+            key={post.id}
+            className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          >
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {post.title}
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              {post.body}
+            </p>
+          </div>
+        )}
+      </div>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -32,8 +32,11 @@ export default function Home() {
         <div className="flex flex-col items-center gap-5 p-5">
           {posts.map((post) => (
             <>
-              <Link key={post.id} href={`/Post/${post.id}`}>
-                <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+              <Link href={`/Post/${post.id}`}>
+                <div
+                  key={post.id}
+                  className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+                >
                   <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                     {post.title}
                   </h5>
